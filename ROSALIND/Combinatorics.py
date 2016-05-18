1b5ec86242930cefb3c5fb82f3425b24f92488cd
Michael Lee <michael_lee92@hotmail.com>
2016-05-18 19:31:25 -0400

    ROSALIND FIBD

--- ROSALIND/Combinatorics.py
+++ ROSALIND/Combinatorics.py
@@ -1,13 +1,25 @@
 
 #FIB: Wascally Wabbits
 
-def getNumRabbits(N, K):
-    pop = {0:0, 1: 1, 2:1}
-    if(N in pop):
-        return pop[N]
-    else:
-        pop[N] = getNumRabbits(N - 1, K) + K * getNumRabbits(N - 2, K)
-        return pop[N]
+def getNumRabbits(N, K): # after n months, each producing k 
+	pop = {0:0, 1: 1, 2:1}
+	if(N in pop):
+		print(pop[N])
+		return pop[N]
+	else:
+		pop[N] = getNumRabbits(N - 1, K) + K * getNumRabbits(N - 2, K)
+		return pop[N]
 
-print(getNumRabbits(29, 2))
-    
+	
+
+#FIBD Wabbit Season
+
+def getNumRabbits(N, M):
+	population = [] # How many rabbits are alive for a generation
+	lifespan = [] # How many more months each generation has to live
+	
+	if(N in pop):
+		print(pop[N])
+		return pop[N] 
+	else:
+		population[N] = getNumRabbits(N - 1, K) + K * getNumRabbits(N - 2, K)
